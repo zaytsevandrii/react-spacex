d3ec2d000e1a26b7b050c5798ca6448ebe68c69e
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-28 18:32:49 +0000

    add main and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <ul>
+            <li>SpaceX &copy; 2022</li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Youtube</a></li>
+            <li><a href="#">Instagramm</a></li>
+            <li><a href="#">Flicker</a></li>
+            <li><a href="#">LinkedIn</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Suppliers</a></li>
+        </ul>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import Footer from "../components/Footer"
 import Header from "../components/Header"
 
 const Home = () => {
@@ -9,12 +11,122 @@ const Home = () => {
                 <div className="section-inner">
                     <h4>Upcoming Launch</h4>
                     <h2>CRS-25 Mission</h2>
-                    <div className="btn animate">
+                    <Link to="#" className="btn animate">
                         <div className="hover"></div>
                         <span>Rewatch</span>
-                    </div>
+                    </Link>
+                </div>
+
+                <div className="scroll-arrow">
+                    <svg width="30px" height="20px">
+                        <path
+                            stroke="#ffffff"
+                            fill="none"
+                            stroke-width="2px"
+                            d="M2.000,5.000 L15.000,18.000 L28.000,5.000 "
+                        ></path>
+                    </svg>
                 </div>
             </section>
+            {/* Section B */}
+            <section className="section-b">
+                <div className="section-inner">
+                    <h4>Recent Launch</h4>
+                    <h2>Starlink Mission</h2>
+                    <Link to="#" className="btn animate">
+                        <div className="hover"></div>
+                        <span>Rewatch</span>
+                    </Link>
+                </div>
+
+                <div className="scroll-arrow">
+                    <svg width="30px" height="20px">
+                        <path
+                            stroke="#ffffff"
+                            fill="none"
+                            stroke-width="2px"
+                            d="M2.000,5.000 L15.000,18.000 L28.000,5.000 "
+                        ></path>
+                    </svg>
+                </div>
+            </section>
+            {/* Section C */}
+            <section className="section-c">
+                <div className="section-inner">
+                    <h4>Recent Mission</h4>
+                    <h2>SES-22 Mission</h2>
+                    <Link to="#" className="btn animate">
+                        <div className="hover"></div>
+                        <span>Rewatch</span>
+                    </Link>
+                </div>
+
+                <div className="scroll-arrow">
+                    <svg width="30px" height="20px">
+                        <path
+                            stroke="#ffffff"
+                            fill="none"
+                            stroke-width="2px"
+                            d="M2.000,5.000 L15.000,18.000 L28.000,5.000 "
+                        ></path>
+                    </svg>
+                </div>
+            </section>
+            {/* Section D */}
+            <section className="section-d">
+                <div className="section-inner">
+                    <h4>Recent Launch</h4>
+                    <h2>Globalstart FM15 Mission</h2>
+                    <Link to="#" className="btn animate">
+                        <div className="hover"></div>
+                        <span>Rewatch</span>
+                    </Link>
+                </div>
+
+                <div className="scroll-arrow">
+                    <svg width="30px" height="20px">
+                        <path
+                            stroke="#ffffff"
+                            fill="none"
+                            stroke-width="2px"
+                            d="M2.000,5.000 L15.000,18.000 L28.000,5.000 "
+                        ></path>
+                    </svg>
+                </div>
+            </section>
+            {/* Section E */}
+            <section className="section-e">
+                <div className="section-inner">
+                    <h2>Starship Update</h2>
+                    <Link to="#" className="btn animate">
+                        <div className="hover"></div>
+                        <span>Rewatch</span>
+                    </Link>
+                </div>
+
+                <div className="scroll-arrow">
+                    <svg width="30px" height="20px">
+                        <path
+                            stroke="#ffffff"
+                            fill="none"
+                            stroke-width="2px"
+                            d="M2.000,5.000 L15.000,18.000 L28.000,5.000 "
+                        ></path>
+                    </svg>
+                </div>
+            </section>
+            {/* Section F */}
+            <section className="section-f">
+                <div className="section-inner">
+                    <h2>Starship to add NASA astronauts on the moon</h2>
+                    <Link to="#" className="btn animate">
+                        <div className="hover"></div>
+                        <span>Rewatch</span>
+                    </Link>
+                </div>
+            </section>
+
+            <Footer/>
         </>
     )
 }
